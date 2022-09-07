788b282ec08b4d7e475e3934d006606b18a1bd9b
Livia Menezes <liviasbm@hotmail.com>
2022-09-06 20:53:46 -0400

    Create Questão5.py
    Questão 5 completa e funcional

---
+++ Questão5.py
@@ -0,0 +1,20 @@
+#Aluna Livia Menezes
+#Escreva uma classe “Quadrado”, crie dois métodos que retornem a
+#área do quadrado e o perímetro, não crie a instância nesse exercício
+
+lado = int(input("Informe a medida do lado do quadrado\n"))
+
+class Quadrado:
+
+    def area(self,lado):
+        area = lado**2
+        return f"A área do quadrado é: {area}"
+
+    def perimetro(self,lado):
+        perimetro=lado*4
+        return f"O perímetro do quadrado é: {perimetro}"
+
+resultado = Quadrado()
+
+print(resultado.area(lado))
+print(resultado.perimetro(lado))
